5c0e35c4f76dd2a7683eae838ed111e136c6a83d
Julian <mrjrhirst@gmail.com>
2023-08-21 17:56:40 +0100

    Base Student class shell provided

---
+++ src/main/java/Student.java
@@ -0,0 +1,39 @@
+package src.main.java;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class Student {
+        String name;
+        String grade;
+        String group;
+        String secretNickName = "MySecretNickName";
+
+        public Student(final String name, final String grade, final String group) {
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getGrade() {
+            return grade;
+        }
+
+        public String getGroup() {
+            return group;
+        }
+
+        public String getSecretNickName() {
+            return secretNickName;
+        }
+
+        upgradeGrade()
+
+
+        downgradeGrade()
+
+
+    }
+
+}
